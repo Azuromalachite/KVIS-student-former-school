5602fe1a9a5d22b50af0d3003c98ea8df7e7f46d
Azuromalachite <33855967+Azuromalachite@users.noreply.github.com>
2019-06-07 22:37:08 +0700

    Bee swarm plot: number of students
    Bee swarm plot; Number of students on y-axis; Separated by region on x-axis

---
+++ visualization/Student_swarm_plot.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 28 12:11:15 2019
+
+@author: azuromalachite
+"""
+
+# Create bee swarm plot: Number of students in 2nd round admission 
+
+
+import matplotlib.pyplot as plt
+import pandas as pd 
+import seaborn as sns
+
+# Load the dataframe
+df = pd.read_csv(r"C:\Users\azuromalachite\Documents\KVIS student\KVIS student dataset.csv")
+
+# Check the column name
+for col in df.columns: 
+    print(col) 
+
+plt.figure(figsize=(8, 10))
+sns.swarmplot(x='Region',y='Number of students in 2nd KVIS admission round (2019)',data = df)
+sns.set(font_scale=1)
+plt.xlabel('Region')
+plt.ylabel('Number of students in 2nd KVIS admission round (2019)')
+plt.show()
+plt.close()
+
